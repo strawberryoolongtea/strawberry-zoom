86b74a4ea179261d0a6fa66b41b1552e3e96d6ec
strawberryoolongtea <strawberrycream@kakao.com>
2022-02-15 02:03:38 +0900

    Create WebSocket events on server and client

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+import WebSocket from "ws";
+
+const http = require("http");
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -12,4 +15,18 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(port, handleListen);
+// app.listen(port, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from Browser 🚫"));
+  socket.on("message", (message) =>
+    console.log(Buffer.from(message, "base64").toString("utf-8"))
+  );
+  socket.send("Hello from server 🍓🍓🍓");
+});
+
+server.listen(port, handleListen);
